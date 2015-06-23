0ac9249eb1ef952332aaefe955361ce816f2d6c9
Andriy Fomin <monolitsoft64@gmail.com>
2015-06-23 07:34:17 +0300

    Изменения от 23.06.2015  7:34:17,48

---
+++ .git_setup/.git_ini.bat
@@ -0,0 +1,23 @@
+rem - настройки локального репозитория и пути к инструментам
+
+set wait=d:\Prj_4\Android\Bin\Utils\wait\wait.exe
+set git_path=c:\Program Files (x86)\Git\
+set git=%git_path%cmd\git.exe
+set prj_path=%~d0%~p0
+
+rem файловые репозитории, или на флешке, или сетевой путь
+set gitfile_1_path=d:\Prj_4\git\git_global\test_4
+set gitfile_2_path=
+set gitfile_3_path=
+
+rem GitHub
+set github_1_path=https://bitbucket.org/Tester64/test1
+set github_2_path=https://bitbucket.org/Tester64/test2
+set github_3_path=
+
+rem BitBucket
+set bitbucket_1_path=https://bitbucket.org/Tester64/test1
+set bitbucket_2_path=
+set bitbucket_3_path=
+
+rem +++
