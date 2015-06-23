ac6445131217e5fbfe7d2a4886ae7753f5f9745d
Andriy Fomin <monolitsoft64@gmail.com>
2015-06-23 11:23:32 +0300

    Изменения от 23.06.2015 11:23:32,87

--- .git_setup/.git_ini.bat
+++ .git_setup/.git_ini.bat
@@ -17,13 +17,13 @@ set gitfile_2_path=
 set gitfile_3_path=
 
 rem GitHub
-set github_1_path=https://github.com/Tester64/test1
+set github_1_path=https://Tester64:L1N0Z1-S5T9C9-Q0Q2R0-G6F6R6@github.com/Tester64/test1
 set github_2_path=https://github.com/Tester64/test2
 set github_3_path=
 
 rem BitBucket
 set bitbucket_1_path=https://bitbucket.org/Tester64/test1
-set bitbucket_2_path=https://Tester64@bitbucket.org/Tester64/test1.git
+set bitbucket_2_path=https://Tester64:Y7Q7C3-X5C6M4-C5H9X3-Y3C1D9@bitbucket.org/Tester64/test1.git
 set bitbucket_3_path=git@bitbucket.org:Tester64/test1.git
 
 rem +++
